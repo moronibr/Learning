cf195ec2f2cc7dc57d0aacd0c8b7d273defbfef4
moronibr <moroniramos@gmail.com>
2024-08-26 11:34:44 -0300

    Third

--- project/controllers/default.go
+++ project/controllers/default.go
@@ -15,7 +15,7 @@ type HelloController struct {
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.vip"
 	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+	c.TplName = "index.html"
 }
 
 func (hello *MainController) SayHello() {
